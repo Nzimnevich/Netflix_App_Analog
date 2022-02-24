e13e151cab703d9a6c5930fcd6b96bac5421d9e4
dragon-bow <69964309+dragon-bow@users.noreply.github.com>
2022-02-24 22:14:28 +0300

    [feature/home-work-1] added Groupie list

---
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/MainContainer.kt
@@ -0,0 +1,29 @@
+package ru.androidschool.intensiv.ui.tvshows
+
+import android.view.View
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.viewbinding.BindableItem
+import ru.androidschool.intensiv.R
+import ru.androidschool.intensiv.data.Movie
+import ru.androidschool.intensiv.databinding.SerialsItemBinding
+
+class MainContainer(
+    private val content: Movie,
+    param: (Any) -> Unit
+) : BindableItem<SerialsItemBinding>() {
+
+    override fun getLayout(): Int = R.layout.serials_item
+
+    override fun bind(view: SerialsItemBinding, position: Int) {
+        view.titleTv.text = content.title
+        view.ratingBarRb.rating = content.rating
+
+        // TODO Получать из модели
+        Picasso.get()
+            .load("https://m.media-amazon.com/images/M/MV5BYTk3MDljOWQtNGI2My00OTEzLTlhYjQtOTQ4ODM2MzUwY2IwXkEyXkFqcGdeQXVyNTIzOTk5ODM@._V1_.jpg")
+            .into(view.imageIv)
+
+    }
+
+    override fun initializeViewBinding(v: View) = SerialsItemBinding.bind(v)
+}
--- app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/tvshows/TvShowsFragment.kt
@@ -1,6 +1,57 @@
 package ru.androidschool.intensiv.ui.tvshows
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import ru.androidschool.intensiv.R
+import androidx.recyclerview.widget.GridLayoutManager
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import ru.androidschool.intensiv.data.MockRepository
+import ru.androidschool.intensiv.databinding.TvShowsFragmentBinding
 
-class TvShowsFragment : Fragment(R.layout.tv_shows_fragment)
+class TvShowsFragment : Fragment() {
+    private var _binding: TvShowsFragmentBinding? = null
+
+    private val binding get() = _binding!!
+
+    private val adapter by lazy {
+        GroupAdapter<GroupieViewHolder>()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = TvShowsFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.recipesRv.layoutManager = GridLayoutManager(context, 1)
+        binding.recipesRv.adapter = adapter.apply { addAll(listOf()) }
+
+        val moviesList =
+            MockRepository.getMovies().map {
+                MainContainer(
+                    it
+                ) { movie -> }
+            }.toList()
+
+        binding.recipesRv.adapter = adapter.apply { addAll(moviesList) }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = TvShowsFragment()
+    }
+}
