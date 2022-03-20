31537bfaafdadf56f5fc5a87f2ebc2ad89331415
dragon-bow <69964309+dragon-bow@users.noreply.github.com>
2022-03-20 18:21:46 +0300

    [feature/home-work-3] fix for comments

--- app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
+++ app/src/main/java/ru/androidschool/intensiv/ui/movie_details/MovieDetailsFragment.kt
@@ -7,7 +7,6 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager.HORIZONTAL
-import androidx.recyclerview.widget.RecyclerView
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import ru.androidschool.intensiv.R
@@ -16,13 +15,6 @@ import ru.androidschool.intensiv.databinding.MovieDetailsFragmentBinding
 
 class MovieDetailsFragment : Fragment(R.layout.movie_details_fragment) {
     private var _binding: MovieDetailsFragmentBinding? = null
-
-    //    private val fotoFirstIV: ImageView by lazy { binding.fotoFirstCircleV }
-//    private val fotoSecondIV: ImageView by lazy { binding.fotoSecondCircleV }
-//    private val fotoThirdIV: ImageView by lazy { binding.fotoThirdCircleV }
-//    private val fotoForthIV: ImageView by lazy { binding.fotoForthCircleV }
-    private val actorsRecyclerView: RecyclerView by lazy { binding.actorsRecyclerView }
-    var ss: String? = null
 
     private val binding get() = _binding!!
 
